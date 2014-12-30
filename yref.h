4b99e95d1a33c792a59c2a62080f1f7dfceb8d06
Yuxuan Shui <yshuiv7@gmail.com>
2014-12-30 03:24:09 -0500

    yref: Finish reference counting

--- yref.h
+++ yref.h
@@ -31,7 +31,6 @@ struct yref;
 typedef struct yref_info {
 	int ref_count;
 	void *start;
-	size_t len;
 	yref_dtor dtor;
 	struct yref_entry *referers;
 #ifndef Y_SINGLE_THREAD
@@ -43,6 +42,7 @@ struct yref_entry {
 	//owner = address of pointer to the ref counted obj
 	void **owner;
 	yref_t *info;
+	bool ret;
 	UT_hash_handle hh;
 };
 #else
@@ -52,22 +52,96 @@ typedef struct yref_info {
 } yref_t;
 #endif
 
+typedef struct yref_ret {
+	void **pp;
+	void *p;
+	yref_t *info;
+} yref_ret_t;
+
+#define yref_def_scope_out_proto(type, member) void type##_scope_out_func(type **);
+
 #define yref_def_scope_out(type, member) void type##_scope_out_func(type **p) {\
 	_yref_unref(p, &(*p)->member); \
 }
 
-#define yref_def_ref(type, name) type *name Y_CLEANUP(type##_scope_out_func)
+#define yref_var(type, name) type *name Y_CLEANUP(type##_scope_out_func)
 
-#define yref_ref(src, dst) _yref_ref(&(dst), src, NULL)
+#define yref_ref(src, dst, member) _yref_ref(&(dst), &(src)->member)
 
-#define yref_move(src, dst)
+#define yref_move(src, dst, member) _yref_move(&(dst), &(src), &(src)->member, false)
 
-#define yref_ref_return(ret)
-#define yref_return_get(expr, dst)
+#define yref_ref_return(ret, member) { \
+	_yref_mark_as_return(&(ret), &(ret)->member); \
+	return ((yref_ret_t){&(ret), (ret), &(ret)->member}); \
+}
+#define yref_return_get(expr, dst, member) { \
+	yref_ret_t __tmp = (expr); \
+	_yref_move((__tmp->pp, &(dst), __tmp->info, true); \
+	dst = __tmp->p; \
+}
+
+static inline void yref_init(void *p, yref_t *r, yref_dtor dtor) {
+	yatomic_set(&r->ref_count, 0);
+	r->dtor = dtor;
+#ifdef YREF_CHECK
+	r->start = p;
+	mtx_init(&r->ref_mtx, 0);
+	r->referers = NULL;
+#else
+	(void)p;
+#endif
+}
+
+/*
+ * _yref_mark_as_return: Mark a referer for returning,
+ * sanity checks will not fail for referers marked as return.
+ * @pp: pointer to the referer
+ * @r: yref_t
+ */
+static inline void _yref_mark_as_return(void **pp, yref_t *r) {
+#ifdef YREF_CHECK
+	mtx_lock(&r->ref_mtx);
+	struct yref_entry *ref;
+	HASH_FIND_PTR(r->referers, &pp, ref);
+	assert(!ref->ret);
+	ref->ret = true;
+	mtx_unlock(&r->ref_mtx);
+#else
+	(void)pp,
+	(void)r;
+#endif
+}
+
+/*
+ * _yref_move: Transfer the ownership from one referer to another
+ * @src: the source referer
+ * @dst: the dest referer
+ * @r: yref_t
+ */
+static inline void _yref_move(void **src, void **dst, yref_t *r,
+			      bool ret) {
+#ifdef YREF_CHECK
+	mtx_lock(&r->ref_mtx);
+	struct yref_entry *ref;
+	HASH_FIND_PTR(r->referers, &src, ref);
+	HASH_DEL(r->referers, ref);
+	assert(ref);
+	assert(ref->ret || *ref->owner == ref->info->start);
+	ref->owner = dst;
+	ref->ret = false;
+	HASH_ADD_PTR(r->referers, owner, ref);
+	mtx_unlock(&r->ref_mtx);
+#endif
+	if (!ret) {
+		//if ret == true, src points to invalid stack area
+		*dst = *src;
+		*src = NULL;
+	}
+}
 
 /*
  * yref_uref: Unref an object, run the dtor if necessery
- * @pp: pointer to a referer
+ * @pp: pointer to the referer
  * @r: yref_t
  * @return: true if the object has zero reference.
  */
@@ -79,10 +153,11 @@ static inline bool _yref_unref(void **pp, yref_t *r) {
 	mtx_lock(&r->ref_mtx);
 	struct yref_entry *ref;
 	HASH_FIND_PTR(r->referers, &pp, ref);
-	assert(ref);
-	assert(*ref->owner == p);
 	HASH_DEL(r->referers, ref);
 	mtx_unlock(&r->ref_mtx);
+	assert(ref);
+	assert(*ref->owner == ref->info->start);
+	free(ref);
 #endif
 	tmp = yatomic_dec(&r->ref_count);
 	assert(tmp >= 0);
@@ -93,7 +168,12 @@ static inline bool _yref_unref(void **pp, yref_t *r) {
 	return false;
 }
 
-static inline void _yref_ref(void **pp, yref_t *r, yref_t *parent) {
+/*
+ * yref_ref: Reference an object
+ * @pp: pointer to the referer
+ * @r: yref_t
+ */
+static inline void _yref_ref(void **pp, yref_t *r) {
 	void *p = r->start;
 	*pp = p;
 #ifdef YREF_CHECK
@@ -101,7 +181,6 @@ static inline void _yref_ref(void **pp, yref_t *r, yref_t *parent) {
 	new_ref->owner = pp;
 
 	mtx_lock(&r->ref_mtx);
-	++r->ref_count;
 	HASH_ADD_PTR(r->referers, owner, new_ref);
 	mtx_unlock(&r->ref_mtx);
 #endif
@@ -110,7 +189,9 @@ static inline void _yref_ref(void **pp, yref_t *r, yref_t *parent) {
 
 static inline void yref_misuse_check(yref_t *r) {
 	struct yref_entry *tmp, *ref;
+	mtx_lock(&r->ref_mtx);
 	HASH_ITER(hh, r->referers, ref, tmp) {
 		assert(*ref->owner == r->start);
 	}
+	mtx_unlock(&r->ref_mtx);
 }
